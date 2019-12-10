10edc82e2495342bbb4d752887176596114c169d
771351 <58735806+771351@users.noreply.github.com>
2019-12-10 08:08:38 -0600

    Add files via upload
    First blank document

---
+++ Mastermind_JAE/Mastermind_JAE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mastermind_JAE
+//
+//  Created by BURRIGHT, JACK on 12/10/19.
+//  Copyright © 2019 BURRIGHT, JACK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Mastermind_JAE/Mastermind_JAETests/Mastermind_JAETests.swift
@@ -0,0 +1,34 @@
+//
+//  Mastermind_JAETests.swift
+//  Mastermind_JAETests
+//
+//  Created by BURRIGHT, JACK on 12/10/19.
+//  Copyright © 2019 BURRIGHT, JACK. All rights reserved.
+//
+
+import XCTest
+@testable import Mastermind_JAE
+
+class Mastermind_JAETests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
